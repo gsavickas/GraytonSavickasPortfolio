07c545f5cc286ec12d6d9fbf0e10359f6f64d683
Grayton Savickas <gsavickaspro@gmail.com>
2021-01-30 19:22:57 -0600

    adds pictures and about page

---
+++ logic.js
@@ -0,0 +1,10 @@
+/*
+============================================
+; Title:  Personal Portfolio
+; Author: Grayton Savickas
+; Date:   29 Jan 2021
+; Modified By:
+; Description: Create a webpage to show off all of my projects
+;===========================================
+*/
+
